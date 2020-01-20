357eee8f3d3187608e430c823cff53bf851f6d40
Yarad <saigopr@yandex.ru>
2020-01-20 15:13:05 +0300

    cron script is ready

--- sheduled_script.php
+++ sheduled_script.php
@@ -8,17 +8,27 @@ include_once __DIR__ . '/DAO/DAOTask.php';
 
 //$client = Asana\Client::accessToken('', ['headers' => ['asana-disable' => 'string_ids']]);
 
-$tasksPDO = DAOTask::getInstance()->getTasksToNotifyAbout();
-if ($tasksPDO) {
+$tasks = DAOTask::getInstance()->getTasksToNotifyAbout();
+if ($tasks) {
     $telegram = new Longman\TelegramBot\Telegram(Constants::get('TELEGRAM_BOT_API'), 'ASANA');
+    $messageTemplate = file_get_contents('templates/messages/simple_message.twig');
 
-    while ($task = $tasksPDO->fetchObject()) {
+    foreach ($tasks as $task) {
         try {
+            //TODO: подключить Twig при более сложных обработках
+            $params = [
+                '{{ link }}' => 'https://app.asana.com/0/0/' . $task->ID_in_asana,
+                '{{ name }}' => $task->name,
+                '{{ notify_time }}' => $task->time_to_notify->format('d.m.Y h:i'),
+                '{{ description }}' => $task->description
+            ];
+
             $response = Request::sendMessage(
                 [
                     'chat_id' => $task->ID_telegram,
-                    'text' => 'NOTIFY',
-                    'timeout' => null,
+                    'text' => str_replace(array_keys($params), array_values($params), $messageTemplate),
+                    'timeout' => 4,
+                    'parse_mode' => 'HTML'
                 ]
             );
             if ($response->isOk()) {
